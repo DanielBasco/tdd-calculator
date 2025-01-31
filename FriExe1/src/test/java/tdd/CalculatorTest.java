52d257671be9f60b394d47d782e036685a661b42
DanielBasco <danielsgeniale@gmail.com>
2025-01-31 12:19:29 +0100

    Add files via upload

---
+++ FriExe1/src/test/java/tdd/CalculatorTest.java
@@ -0,0 +1,25 @@
+package tdd;
+
+import org.junit.jupiter.api.AfterEach;
+import  org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void testSomething(){
+      String actual = "";
+      String expected = "";
+      assertEquals(actual, expected);
+    }
+}
